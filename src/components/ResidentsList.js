2d49720f378ef0eb6d3b71773492781445a3c167
Zeta7 <edson.joan.m.o97@gmail.com>
2022-03-14 20:05:51 -0500

    ordenamiento css

--- src/components/ResidentsList.js
+++ src/components/ResidentsList.js
@@ -4,15 +4,15 @@ import ResidentInfo from './ResidentInfo';
 const ResidentsList = ({list = []}) =>{
     //console.log(list);
     return(
-        <>
+        <div  className='cont_list'>
             {
                 list.map((item, index)=>(
-                    <div key={index} className='cont_list'>
+                    <div key={index}>
                         <ResidentInfo link={item}/>
                     </div>
                 ))
             }
-        </>
+        </div>
     );
 }
 
